5bbf8bcb920b87bd91989e304510ee894ef11f25
Didcvee <8anibus8@gmail.com>
2023-11-27 16:34:55 +0300

    first commit

---
+++ git-config-server/src/test/java/ru/didcvee/micro/gitconfigserver/GitConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.didcvee.micro.gitconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
